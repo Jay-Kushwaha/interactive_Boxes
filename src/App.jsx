032ad42700c9af9b7644406fd7673c6ccdb954f7
jk <jk@kashees-MacBook-Air.local>
2025-06-28 12:35:23 +0530

    head text

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Head from "./components/Head"
+function App() {
+  return (
+    <>
+   <Head/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Head.jsx
@@ -0,0 +1,8 @@
+const Head = () =>{
+    return (
+        <div className="head">
+           <p><span><hr/></span>YAY! its BOGO<span><hr/></span></p>
+        </div>
+    )
+}
+export default Head;
